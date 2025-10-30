634a19bbc501a5968147f6e59441d91717b99ed7
PRATHMESH BADHE <wdwwc@sinhgad.edu>
2025-10-30 18:47:17 +0530

    done with L1

---
+++ Basics_1_to_5/homework_1.cpp
@@ -0,0 +1,87 @@
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     int p , r , t;
+//     cout << "Enter the value of principle amount :";
+//     cin >> p;
+//     cout << "Enter the value of time amount :";
+//     cin >> t;
+//     cout << "Enter the value of rate amount :";
+//     cin >> r;
+//     float SI = p * r * t ;
+//     cout << "The value of SI is " << SI << endl;
+//     return 0;
+// }
+
+
+
+
+
+
+
+
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     int a , b;
+//     cout << "Enter the first number :";
+//     cin >> a;
+//     cout << "enter the second number :";
+//     cin >> b;
+//     if(a > b){
+//         cout << "max number is " << a << endl;
+//     }
+//     else{
+//         cout << "max is " << b;
+//     }
+//     return 0;
+// }
+
+
+
+
+
+
+
+
+
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     int n ;
+//     cout << "Enter the number :";
+//     cin >> n;
+//     int facto = 1;
+//     for(int i = 1 ; i <= n ; i++){
+//         facto *= i;
+//     }
+//     cout << facto << endl;
+//     return 0;
+// }
+
+
+
+
+
+
+
+
+
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     int age;
+//     cout << "Enter your age here :";
+//     cin >> age;
+//     if (age >= 18){
+//         cout << "You can get a driving license" << endl;
+//     }
+//     else{
+//         cout << "You cannot get a driving license" << endl;
+//     }
+//     return 0;
+// }
