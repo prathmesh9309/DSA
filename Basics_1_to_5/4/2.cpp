3f00b0fe9d8b655064289f767960f4f8540d29ed
PRATHMESH BADHE <wdwwc@sinhgad.edu>
2025-11-02 11:59:58 +0530

    triangle_pattern

--- Basics_1_to_5/4/2.cpp
+++ Basics_1_to_5/4/2.cpp
@@ -18,3 +18,26 @@ int main() {
     }
     return 0;
 }
+
+
+
+/*
+ABC
+DEF
+GHI
+*/
+#include <iostream>
+using namespace std;
+
+int main() {
+    char ch = 'A';
+    int n = 3;
+    for(int i = 0 ; i < n ; i++){
+        for(int j = 0 ; j < n ; j++){
+            cout << ch << " ";
+            ch ++;
+        }
+        cout << endl;
+    }
+    return 0;
+}
